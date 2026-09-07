1d053f9bdee4aff6c2b06cf370aaecbf67c7830d
Walter Andrade <walteraandrade@gmail.com>
2026-09-07 13:28:28 -0300

    Add camara collector for floor-speech summaries, opt-in

--- src/collectors/index.ts
+++ src/collectors/index.ts
@@ -4,6 +4,7 @@ import { gdelt } from './gdelt.js'
 import { rss } from './rss.js'
 import { gnews } from './gnews.js'
 import { gkg } from './gkg.js'
+import { camara } from './camara.js'
 
-export const collectors: Record<Source, Collector> = { bluesky, gdelt, rss, gnews, gkg }
+export const collectors: Record<Source, Collector> = { bluesky, gdelt, rss, gnews, gkg, camara }
 export const defaultSources: Source[] = ['bluesky', 'rss', 'gnews', 'gkg']
